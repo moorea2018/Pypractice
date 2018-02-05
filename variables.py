350cd516cbb2c24398d4ba4dcd49e0cb40374205
moorea2018 <amoore@sjnma.org>
2018-02-05 10:19:47 -0600

    python code

---
+++ variables.py
@@ -0,0 +1,13 @@
+print ("hello world!")
+print ("HELLO WORLD!")
+print ('''I 
+AM
+THE 
+GOAT''')
+first_name = raw_input("what is your first name?")
+
+last_name = raw_input("what is your last name?")
+
+full_name = first_name +" "+ last_name
+
+print("welcome to my crib, "+full_name)
